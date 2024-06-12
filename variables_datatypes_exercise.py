953f572e78fd676c29e1168877a152bdd8e273fc
waltf4code <waltf4@hotmail.com>
2024-06-12 00:20:36 -0300

    Variables & Datatypes - Exercise

---
+++ variables_datatypes_exercise.py
@@ -0,0 +1,9 @@
+print('Variables & Datatypes - Exercise')
+# Create appropriate Variables for Item name, the price
+# and how many you have in stock
+
+item_name = "widget"
+price = 23.5
+inventory = 100
+is_in_inventory = True
+print(item_name, price, inventory)
