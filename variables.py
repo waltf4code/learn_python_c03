c371bbefad8543484460426d3dea9c925ffd8de9
waltf4code <waltf4@hotmail.com>
2024-06-11 23:55:08 -0300

    Variables

---
+++ variables.py
@@ -0,0 +1,7 @@
+failed_subjects = "6"
+name = "John"
+print('Dear Mr Badger')
+print('Your son Eric is falling ' + failed_subjects + ' subjects')
+print(name + ' will need to redo ' + failed_subjects + ' courses')
+name = 'Eric'
+print(name + ' is doing well in geography')
