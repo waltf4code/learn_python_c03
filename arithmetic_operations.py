16112c64ff629d2eb584c8af9c1cd982f7fa2ef1
waltf4code <waltf4@hotmail.com>
2024-06-14 23:44:26 -0300

    Arithmetic Operations

---
+++ arithmetic_operations.py
@@ -0,0 +1,9 @@
+a=6
+b=2
+print(f"Addition : {a+b}")
+print(f"Subtration: {a-b}")
+print(f"Multiplication: {a+b}")
+print(f"Division float: {a/b}")
+print(f"Division floor: {a//b}")
+print(f"Modulus {a%b}")
+print(f"Exponent {a**b}")
